5c8edf3bb15c517eb709ffc5b86b6469fd4d21cc
zohaib hassan <hassanzohaib382@gmail.com>
2024-03-05 09:55:44 +0500

    3 days coding done

---
+++ coding/main.ts
@@ -0,0 +1,83 @@
+
+
+
+
+
+// day one challeng start conding !
+
+// // Q2
+
+//    let personName : string = "zohaib hassan";
+
+//    console.log(`hello ${personName},would you like to learn typescript today?`);
+  
+
+// Q3
+
+// 01
+
+//  let personName : string = "ZOHAIB HASSAN";
+
+// console.log(personName.toLowerCase());
+
+// 02
+
+//  let personName : string = "zohaib hassan";
+//  console.log(personName.toUpperCase());
+
+// 03
+
+//  let personName : string = "zohaib hassan";
+//  console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
+
+
+// day two challeng start conding !
+
+// Q4
+  // console.log(`"Albert Einstein one said,"A person who never mistake a tried any thing new."`);
+
+// Q5
+
+      // let famous_person : string = "albert Einstein"
+      // let massage : string = `${famous_person} one said, "a person who never mistake a tried anything new."`;
+      //  console.log(massage); 
+
+// Q6
+
+//  let personName : string = "\t\n zohaib \n\t" ;
+//  console.log(personName);
+
+//  console.log(personName.trim());
+
+
+
+
+
+  
+
+// day three coding start!
+
+
+// Q7
+// Q8
+
+// console.log(4+4);
+// console.log(12-4);
+// console.log(2*4);
+// console.log(16/2);
+
+
+// Q9
+
+ let favoriteNumber : number = 24 ;
+ console.log(`my favorite number is ${favoriteNumber}`);
+
+
+
+
+
+
+
+
+
+
